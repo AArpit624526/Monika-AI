5eca7e77bd71dcfb9458773cd1bae1f5964e17af
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-03-31 23:21:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.post("/ask", async (req, res) => {
 app.post("/voice", async (req, res) => {
   const { text } = req.body;
   const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
-  const VOICE_ID = "Pt5YrLNyu6d2s3s4CVMg"; 
+  const VOICE_ID = "m7GHBtY0UEqljrKQw2JH"; // Aisha Voice's
 
   try {
     const response = await axios({

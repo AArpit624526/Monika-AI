f18ad9a7167d66d295178dd017ec4ada1f381cb7
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-04-23 00:44:43 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'monika-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (!event.request.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
